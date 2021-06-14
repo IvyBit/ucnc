40f24866b1efd148717c19d89f190df5aab91dc4
Alexander Hammer <35197182+IvyBit@users.noreply.github.com>
2021-06-14 23:12:18 +0200

    Yeetus deletus

--- firmware/ucnc/headers/avrtypes.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-
-typedef uint16_t avr_size_t;
--- firmware/ucnc/headers/errors.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "avrtypes.h"
-
-namespace err {
-
-	__attribute__((noreturn)) extern void on_error(uint8_t ec);
-	
-};
